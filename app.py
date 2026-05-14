7708a11fefdb08f24687662033441e7a357188c8
HamdiMohamedElKhames <hammahamdi2000@gmail.com>
2026-05-14 01:39:11 +0100

    app changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import math
+import random
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
